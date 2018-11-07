4287d6598a6100532bdfc4b26e9e8b214202d5be
Ezequiel Contarino <econtarino@conicet.gov.ar@18-013.devel.conicet.gov.ar>
2018-11-07 09:42:37 -0300

    se sube curso de node

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('Hola mundo');
+var Emitter = require('./emitter')
+
+var emtr = new Emitter();
+
+emtr.on('greet',function (){
+    console.log('Somewhere, someone said hello.')
+})
+
+emtr.on('greet',function(){
+    console.log('A greeting occurred!');
+})
+console.log('Hello')
+emtr.emit('greet')
