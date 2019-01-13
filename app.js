0f3c3363a3bf609f518338c0b912b5e7d51ef6b0
eze.contarino7@gmail.com <eze.contarino7@gmail.com>
2019-01-13 17:20:32 -0300

    se sube version con json

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 var http = require('http');
 var fs =   require('fs');
 
-http.createServer(function (req,res){
-    res.writeHead(200,{'Content-Type':'text/html'});
-    var html = fs.readFileSync(__dirname+'/index.html');
-    res.end(html);
-}).listen(8080,'127.0.0.1');
+//http.createServer(function (req,res){
+//   res.writeHead(200,{'Content-Type':'text/html'});
+//    var html = fs.readFileSync(__dirname+'/index.html');
+//    res.end(html);
+//}).listen(8080,'127.0.0.1');
+
+
+http.createServer(req, res  => {
+    res.writeHead(200, { 'Content-Type': 'text/json' });
+    var persona = {
+        name: 'Ezequiel',
+        age: 27
+    };
+    res.end(JSON.stringify(persona));
+}).listen(8080, '127.0.0.1');
